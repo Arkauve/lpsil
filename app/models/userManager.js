dced26a0671420ed65a8c8218d60f39b9c77961d
Romain garnier <garnierr10@gmail.com>
2017-01-22 16:01:10 +0100

    modifcation du profil, enregistrement des modifcations dans la base de données
    - suppression de la modification du mot de passe
    - ajout de la méthode d'enregistrement des modifications dans
    userManager.js

--- app/models/userManager.js
+++ app/models/userManager.js
@@ -3,83 +3,150 @@ var logger = require('log4js').getLogger('Server');
 
 module.exports = {
     login: function(login, pwd, callback) {
-        var res = global.connection.query("Select * from users where email='" + login + "' and password='" + pwd + "'", function(err, rows) {
+        var res = global.connection.query("Select * from users where u_email='" + login + "' and u_password='" + pwd + "'", function(err, rows) {
             if (!err) {
-                callback(rows.length == 1,rows[0]);
-            } else{
+                logger.info("L'id du resultat de la requête: ", rows[0].u_id);
+                callback(rows.length == 1, rows[0]);
+            } else {
                 logger.info('Une erreur est survenue :  ', err);
                 callback(err);
-              }
-        });
-    },
-    register: function(email,password,nom,prenom,tel,website,sexe,birthdate,ville,taille,couleur,profilepic,callback){
-      if(nom=="")
-        nom=null;
-      else {
-        nom="'"+nom+"'";
-      }
-      if(prenom=="")
-        prenom=null;
-      else {
-        prenom="'"+prenom+"'";
-      }
-      if(tel=="")
-        tel=null;
-      else {
-        tel="'"+tel+"'";
-      }
-      if(website=="")
-        website=null;
-      else {
-        website="'"+website+"'";
-      }
-      if(sexe=="")
-        sexe=null;
-      else {
-        sexe="'"+sexe+"'";
-      }
-      if(birthdate=="")
-        birthdate=null;
-      else {
-        birthdate="'"+birthdate+"'";
-      }
-      if(ville=="")
-        ville=null;
-      else {
-        ville="'"+ville+"'";
-      }
-      if(taille=="")
-        taille=null;
-
-      if(couleur=="")
-        couleur=null;
-      else{
-        couleur="'"+couleur.substring(1)+"'";
-      }
-      if(profilepic=="")
-        profilepic=null;
-      else {
-        profilepic="'"+profilepic+"'";
-      }
-      var res = global.connection.query("INSERT INTO users (email, password, nom, prenom, tel, website, sexe, birthdate, ville, taille, couleur, profilepic) "+
-      " VALUES('"+email+"','"+password+"',"+nom+","+prenom+","+tel+","+website+","+sexe+","+birthdate+","+ville+","+taille+","+couleur+","+profilepic+")", function(err, rows) {
-          if (!err) {
-              callback(true);
-          } else{
-              logger.info('Une erreur est survenue :  ', err);
-              callback(err);
             }
-      });
+        });
     },
-    getUser : function(email, callback) {
-        var res = global.connection.query("Select * from users where email='" + email + "'", function(err, rows) {
+    register: function(email, password, nom, prenom, tel, website, sexe, birthdate, ville, taille, couleur, profilepic, callback) {
+        if (nom == "")
+            nom = null;
+        else {
+            nom = "'" + nom + "'";
+        }
+        if (prenom == "")
+            prenom = null;
+        else {
+            prenom = "'" + prenom + "'";
+        }
+        if (tel == "")
+            tel = null;
+        else {
+            tel = "'" + tel + "'";
+        }
+        if (website == "")
+            website = null;
+        else {
+            website = "'" + website + "'";
+        }
+        if (sexe == "")
+            sexe = null;
+        else {
+            sexe = "'" + sexe + "'";
+        }
+        if (birthdate == "")
+            birthdate = null;
+        else {
+            birthdate = "'" + birthdate + "'";
+        }
+        if (ville == "")
+            ville = null;
+        else {
+            ville = "'" + ville + "'";
+        }
+        if (taille == "")
+            taille = null;
+
+        if (couleur == "")
+            couleur = null;
+        else {
+            couleur = "'" + couleur.substring(1) + "'";
+        }
+        if (profilepic == "")
+            profilepic = null;
+        else {
+            profilepic = "'" + profilepic + "'";
+        }
+        var res = global.connection.query("INSERT INTO users (u_email, u_password, u_nom, u_prenom, u_tel, u_website, u_sexe, u_birthdate, u_ville, u_taille, u_couleur, u_profilepic) " +
+            " VALUES('" + email + "','" + password + "'," + nom + "," + prenom + "," + tel + "," + website + "," + sexe + "," + birthdate + "," + ville + "," + taille + "," + couleur + "," + profilepic + ")",
+            function(err, rows) {
+                if (!err) {
+                    callback(true);
+                } else {
+                    logger.info('Une erreur est survenue :  ', err);
+                    callback(err);
+                }
+            });
+    },
+    getUser: function(email, callback) {
+        var res = global.connection.query("Select * from users where u_email='" + email + "'", function(err, rows) {
             if (!err) {
-                logger.info("L'id du resultat de la requête: ", rows[0].id);
-                callback(rows.length == 1,rows[0]);
-            } else{
+                logger.info("L'id du resultat de la requête: ", rows[0].u_id);
+                logger.info('date :  ', rows[0].birthdate);
+                callback(rows.length == 1, rows[0]);
+            } else {
                 logger.info('Une erreur est survenue :  ', err);
                 callback(err);
-              }
+            }
         });
+    },
+    editProfile: function(id, email, nom, prenom, tel, website, sexe, birthdate, ville, taille, couleur, profilepic, callback) {
+        if (nom == "")
+            nom = null;
+        else {
+            nom = "'" + nom + "'";
+        }
+        if (prenom == "")
+            prenom = null;
+        else {
+            prenom = "'" + prenom + "'";
+        }
+        if (tel == "")
+            tel = null;
+        else {
+            tel = "'" + tel + "'";
+        }
+        if (website == "")
+            website = null;
+        else {
+            website = "'" + website + "'";
+        }
+        if (sexe == "")
+            sexe = null;
+        else {
+            sexe = "'" + sexe + "'";
+        }
+        if (birthdate == "")
+            birthdate = null;
+        else {
+            birthdate = "'" + birthdate + "'";
+        }
+        if (ville == "")
+            ville = null;
+        else {
+            ville = "'" + ville + "'";
+        }
+        if (taille == "")
+            taille = null;
+
+        if (couleur == "")
+            couleur = null;
+        else {
+            couleur = "'" + couleur.substring(1) + "'";
+        }
+        if (profilepic == "")
+            profilepic = null;
+        else {
+            profilepic = "'" + profilepic + "'";
+        }
+        var requete = "Update users SET u_email='" + email + "', u_nom=" + nom + ", u_prenom=" + prenom + ", u_tel=" + tel +
+        ", u_website=" + website + ", u_sexe=" + sexe + ", u_birthdate=" + birthdate + ", u_ville=" + ville +
+        ", u_taille=" + taille + ", u_couleur=" + couleur + ", u_profilepic=" + profilepic +
+        " WHERE u_id=" + id;
+        var res = global.connection.query(requete,
+            function(err, rows) {
+                if (!err) {
+                    callback(true);
+                } else {
+                    logger.info('Une erreur est survenue lors de la requete: ', requete);
+                    logger.info('ERREUR : ', err);
+                    callback(err);
+                }
+            });
     }
 }
--- app/root.js
+++ app/root.js
@@ -16,34 +16,36 @@ var connection = mysql.createConnection({
 var express = require('express');
 var root = express();
 
-root.get('/', function(req, res){
+root.get('/', function(req, res) {
     res.redirect('/login');
 });
 
-root.get('/login', function(req, res){
+root.get('/login', function(req, res) {
     res.render('login');
 });
 
-root.post('/login', function (req, res) {
-    userManager.login(req.body.username,req.body.password, function(clb, row){
-      logger.info("connection:",clb);
-      if(clb==true){
-        session.open = true;
-        session.id = row.id;
-        session.email = row.email;
-        session.nom = row.nom;
-        session.prenom = row.prenom;
-        session.sexe = row.sexe;
-        session.tel = row.tel;
-        session.birthdate = row.birthdate;
-        session.taille = row.taille;
-        session.couleur = row.couleur;
-        session.website = row.website;
-        session.profilepic = row.profilepic;
-        res.redirect('/profile');
-      }
-      else
-        res.render('login',{error:"identifiant ou mot de passe incorrect"});
+root.post('/login', function(req, res) {
+    userManager.login(req.body.username, req.body.password, function(clb, row) {
+        logger.info("connection:", clb);
+        if (clb == true) {
+            session.open = true;
+            session.id = row.u_id;
+            session.email = row.u_email;
+            session.nom = row.u_nom;
+            session.prenom = row.u_prenom;
+            session.sexe = row.u_sexe;
+            session.tel = row.u_tel;
+            session.birthdate = row.u_birthdate;
+            session.ville = row.u_ville;
+            session.taille = row.u_taille;
+            session.couleur = row.u_couleur;
+            session.website = row.u_website;
+            session.profilepic = row.u_profilepic;
+            res.redirect('/profile');
+        } else
+            res.render('login', {
+                error: "identifiant ou mot de passe incorrect"
+            });
     });
     /*connection.connect();
     connection.query('SELECT nom, prenom from users where email="'+req.body.username+'" and password="'+req.body.password+'"', function (err, rows, fields) {
@@ -58,85 +60,121 @@ root.post('/login', function (req, res) {
     });*/
 });
 
-root.get('/register', function (req, res) {
+root.get('/register', function(req, res) {
     res.render('register');
 });
 
-root.post('/register',function(req,res){
-  userManager.register(req.body.email, req.body.password, req.body.nom, req.body.prenom, req.body.tel, req.body.website, req.body.sexe, req.body.birthdate, req.body.ville, req.body.taille, req.body.couleur, req.body.profilepic, function(clb){
-    logger.info("connection:",clb);
-    if(clb==true){
-      userManager.getUser(req.body.email,function(clb, row){
-        if(clb==true){
-          session.open = true;
-          session.id = row.id;
-          session.email = row.email;
-          session.nom = row.nom;
-          session.prenom = row.prenom;
-          session.sexe = row.sexe;
-          session.tel = row.tel;
-          session.birthdate = row.birthdate;
-          session.taille = ro.taille;
-          session.couleur = row.couleur;
-          session.website = row.website;
-          session.profilepic = row.profilepic;
-          res.redirect('/profile');
-        }
-        else
-          res.render('register',{error:"une erreur s'est produite"});
-      });
-    }
-    else
-      res.render('register',{error:"une erreur s'est produite"});
-  });
+root.post('/register', function(req, res) {
+    userManager.register(req.body.email, req.body.password, req.body.nom, req.body.prenom, req.body.tel, req.body.website, req.body.sexe, req.body.birthdate, req.body.ville, req.body.taille, req.body.couleur, req.body.profilepic, function(clb) {
+        logger.info("connection:", clb);
+        if (clb == true) {
+            userManager.getUser(req.body.email, function(clb, row) {
+                if (clb == true) {
+                    session.open = true;
+                    session.id = row.u_id;
+                    session.email = row.u_email;
+                    session.nom = row.u_nom;
+                    session.prenom = row.u_prenom;
+                    session.sexe = row.u_sexe;
+                    session.tel = row.u_tel;
+                    session.birthdate = row.u_birthdate;
+                    session.ville = row.u_ville;
+                    session.taille = row.u_taille;
+                    session.couleur = row.u_couleur;
+                    session.website = row.u_website;
+                    session.profilepic = row.u_profilepic;
+                    res.redirect('/profile');
+                } else
+                    res.render('register', {
+                        error: "une erreur s'est produite"
+                    });
+            });
+        } else
+            res.render('register', {
+                error: "une erreur s'est produite"
+            });
+    });
 });
 
 //redirection vers le profil de l'utilisateur
-root.get('/profile', function (req, res) {
-  if(session.open){
-      res.render('profile',{
-        id : session.id,
-        email : session.email,
-        nom : session.nom,
-        prenom : session.prenom,
-        sexe : session.sexe,
-        tel : session.tel,
-        birthdate : session.birthdate,
-        website : session.website,
-        taille : session.taille,
-        couleur : session.couleur,
-        profilepic : session.profilepic
-      });
-  }
-  else {
-    res.redirect('/login');
-  }
+root.get('/profile', function(req, res) {
+    if (session.open) {
+        res.render('profile', {
+            id: session.id,
+            email: session.email,
+            nom: session.nom,
+            prenom: session.prenom,
+            sexe: session.sexe,
+            tel: session.tel,
+            birthdate: session.birthdate,
+            website: session.website,
+            ville: session.ville,
+            taille: session.taille,
+            couleur: session.couleur,
+            profilepic: session.profilepic
+        });
+    } else {
+        res.redirect('/login');
+    }
 });
 
-root.get('/editProfile', function (req, res) {
-  if(session.open){
-      res.render('editProfile',{
-        email : session.email,
-        nom : session.nom,
-        prenom : session.prenom,
-        sexe : session.sexe,
-        tel : session.tel,
-        birthdate : session.birthdate,
-        website : session.website,
-        taille : session.taille,
-        couleur : session.couleur,
-        profilepic : session.profilepic
-      });
-  }
-  else {
-    res.redirect('/login');
-  }
+root.get('/editProfile', function(req, res) {
+    if (session.open) {
+        res.render('editProfile', {
+            id: session.id,
+            email: session.email,
+            nom: session.nom,
+            prenom: session.prenom,
+            sexe: session.sexe,
+            tel: session.tel,
+            birthdate: session.birthdate,
+            website: session.website,
+            ville: session.ville,
+            taille: session.taille,
+            couleur: session.couleur,
+            profilepic: session.profilepic
+        });
+    } else {
+        res.redirect('/login');
+    }
 });
 
-root.get('/logout', function (req, res) {
-  session.open=false;
-  session.id=null;
-  res.redirect('/login');
+root.post('/editProfile', function(req, res) {
+    userManager.editProfile(session.id, req.body.email, req.body.nom, req.body.prenom, req.body.tel, req.body.website, req.body.sexe, req.body.birthdate, req.body.ville, req.body.taille, req.body.couleur, req.body.profilepic, function(clb) {
+        logger.info("connection:", clb);
+        if (clb == true) {
+            userManager.getUser(req.body.email, function(clb, row) {
+                if (clb == true) {
+                    session.open = true;
+                    session.id = row.u_id;
+                    session.email = row.u_email;
+                    session.nom = row.u_nom;
+                    session.prenom = row.u_prenom;
+                    session.sexe = row.u_sexe;
+                    session.tel = row.u_tel;
+                    session.birthdate = row.u_birthdate;
+                    session.ville = row.u_ville;
+                    session.taille = row.u_taille;
+                    session.couleur = row.u_couleur;
+                    session.website = row.u_website;
+                    session.profilepic = row.u_profilepic;
+                    res.redirect('/profile');
+                } else
+                    res.render('editProfile', {
+                        error: "une erreur s'est produite"
+                    });
+            });
+        } else
+            res.render('editProfile', {
+                error: "une erreur s'est produite"
+            });
+    });
+});
+
+root.get('/logout', function(req, res) {
+    session.open = false;
+    session.id = null;
+    res.redirect('/login');
 });
 
 module.exports = root;
